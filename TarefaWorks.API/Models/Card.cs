b8c399f94096efa77b328e96633d4a487fe64a24
victor <victor@gmail.com>
2022-06-08 09:09:38 -0300

    Inclusão da propriedade DataPrevista

--- TarefaWorks.API/Models/Card.cs
+++ TarefaWorks.API/Models/Card.cs
@@ -3,14 +3,15 @@ namespace TarefaWorks.API.Models
 {
     public class Card
     {
-        public Card(int id, string projeto, string objetivo, string descricao, double tempoPrevisto, string equipe)
+        public Card(int id, string projeto, string objetivo, string descricao, DateTime dataPrevista, double tempoPrevisto, string equipe)
         {
             Id = id;
             Projeto = projeto;
             Objetivo = objetivo;
             Descricao = descricao;
+            DataPrevista = dataPrevista;
             TempoPrevisto = tempoPrevisto;
-            TempoSaldo = TempoSaldo - TempoPrevisto;
+            TempoSaldo = DateTime.Now.Hour - TempoPrevisto;
             Equipe = equipe;
             Status = "Aguardando";
         }
@@ -19,6 +20,7 @@ namespace TarefaWorks.API.Models
         public string Projeto { get; set; }
         public string Objetivo { get; set; }
         public string Descricao { get; set; }
+        public DateTime DataPrevista { get; set; }
         public double TempoPrevisto { get; set; }
         public double TempoSaldo { get; set; }
         public string Equipe { get; set; }
